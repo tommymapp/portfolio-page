9538cbf579e96e6bc3ea3de80a1548e593f39366
arkii1 <tommy.mapp@hotmail.com>
2025-12-14 12:09:38 +0000

    Hide animated sheets on screen readers

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,10 @@ export default function RootLayout({
         <Navbar />
 
         <main className="flex flex-col items-center justify-center h-screen w-screen p-s max-w-[720px]">{children}</main>
-        <div className="absolute h-screen w-full overflow-hidden bg-secondary/50 flex items-center justify-center -bottom-1/4 pointer-events-none">
+        <div 
+          className="absolute h-screen w-full overflow-hidden bg-secondary/50 flex items-center justify-center -bottom-1/4 pointer-events-none"
+          aria-hidden="true"
+          >
             <div className="sheet sheet-1" />
             <div className="sheet sheet-2" />
             <div className="sheet sheet-3" />
