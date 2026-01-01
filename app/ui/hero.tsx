dc37c7a99111d8c552ce4f159c44c3b8bfcf8ad0
arkii1 <tommy.mapp@hotmail.com>
2026-01-01 00:15:48 +0000

    add a splash of ~colour~

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -9,8 +9,8 @@ import FloatingAction from "./components/floatingAction";
 export default function Hero() {
   return (
     <section className="flex flex-col items-start justify-center h-screen" aria-labelledby="hero-title" role="region">
-          <h1 id="hero-title">Hi, I'm Tommy!</h1>
-          <p className="opacity-50 mt-2">A full-stack developer certified in AWS (SAA-003) who loves building and learning cool stuff. I'm based in Bristol - you can find more about me below :)</p>  
+          <h1 id="hero-title">Hi, I'm <span className="text-accent">Tommy!</span></h1>
+          <p className="opacity-50 mt-2">A full-stack web developer certified in AWS (SAA-003) who loves building and learning cool stuff. I'm based in Bristol - you can find more about me below :)</p>  
           <div className="flex justify-start gap-m pl-0 pt-2xs">
             <IconLink link="https://www.linkedin.com/in/tommymapp/" icon={FaLinkedin} classes='opacity-75' ariaLabel="Go to Tommy's LinkedIn page" />
             <IconLink link="https://github.com/arkii1" icon={FaGithub} classes='opacity-75' ariaLabel="Go to Tommy's GitHub page"/>
