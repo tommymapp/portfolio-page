5c4efea5018fc534b31ac6d90c2992088d16fbfa
arkii1 <tommy.mapp@hotmail.com>
2026-01-03 11:42:13 +0000

    Add text to floating action & improve mobile animation when scrolling

--- app/ui/components/floatingAction.tsx
+++ app/ui/components/floatingAction.tsx
@@ -5,17 +5,25 @@ import { useEffect, useState } from 'react'
 
 export default function FloatingAction() {
   const [atProjects, setAtProjects] = useState(false)
+  const [currentText, setCurrentText] = useState('View Projects')
+
+
 
   useEffect(() => {
+    const isMobile = window.innerWidth < 640
     const projects = document.getElementById('projects')
-    if (!projects) return
+    if(!projects)
+      throw new Error('No project section found')
 
     const observer = new IntersectionObserver(
       ([entry]) => {
         setAtProjects(entry.isIntersecting)
+        const text:string = entry.isIntersecting ? 'Return Home' : 'View Projects'
+        setCurrentText(text)
       },
       {
-        threshold: 0.4,
+        threshold: 0,
+        rootMargin: isMobile ? '-20% 0px -60% 0px' : '0% 0px 0% 0px'
       }
     )
 
@@ -38,17 +46,22 @@ export default function FloatingAction() {
       onClick={onClick}
       aria-label={atProjects ? 'Scroll to top' : 'Scroll to projects'}
       className="
+        px-xs
+        py-2xs
+        gap-2.5
+        bg-secondary-500 text-primary-500
+        rounded-full 
+        flex items-center
         fixed -right-s mr-l bottom-s
         cursor-pointer
         animate-bob
         z-50
       "
     >
+      {currentText}
       <ArrowDownIcon
         className={`
-          h-l w-l
-          bg-secondary-500 text-primary-500
-          rounded-full p-4xs
+          h-m w-m
           transition-transform duration-500 ease-in-out
           ${atProjects ? '-rotate-180' : 'rotate-0'}
         `}
